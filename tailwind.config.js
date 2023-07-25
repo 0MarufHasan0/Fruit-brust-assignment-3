cb2712d3adc7d4adf3505f6694d6c45c5210d3dc
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2023-07-26 01:15:45 +0600

    banner edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,4 @@ module.exports = {
     },
     plugins: [],
   }
-  module.exports = {
-    theme: {
-      extend: {
-        backgroundImage: {
-          'team-section': "url('./images/member1.png')",
-          'footer-texture': "url('/img/footer-texture.png')",
-        }
-      }
-    }
-  }
   
